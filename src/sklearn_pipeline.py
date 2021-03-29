008a8ca38daac9e0540b72a875252dba935acce9
leckieje <leckieje@gmail.com>
2021-03-29 17:02:47 -0400

    sklearn pipeline

---
+++ src/sklearn_pipeline.py
@@ -0,0 +1,125 @@
+# sklearn pipeline 
+import pandas as pd 
+import numpy as np
+import random
+import string
+import re
+import nltk
+from nltk.corpus import stopwords, words
+from nltk.corpus import words
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+# nltk.download('words')
+
+# english dictionary 
+eng_dict = words.words()
+
+# get a random sample of chyrons
+def get_random_sample(df, num_samples):
+    idx_lst = [num for num in range(len(df))]
+    idx_samps = random.sample(idx_lst, num_samples)
+    df_samp = df.iloc[idx_samps]
+    return df_samp
+
+# get stop words
+def get_stop_words(words):
+    sw = stopwords.words('english')
+    stops = words[0]
+    
+    for lst in words[1:]:
+        stops += lst
+    
+    return sw + stops
+
+# sklearn count vectorizer
+def get_countvec(corpus, stop_words='english', min_df=20):
+    vectorizer = CountVectorizer(stop_words=stop_words, min_df=min_df)
+    X = vectorizer.fit_transform(corpus)
+    feature_names = vectorizer.get_feature_names()
+    
+    return feature_names, X.toarray()
+
+# sklearn tfidf vectorizer
+def get_tfidf(corpus, max_features=None, min_df=20):
+    vectorizer = TfidfVectorizer(max_features=None, min_df=min_df)
+    X = vectorizer.fit_transform(corpus)
+    feature_names = vectorizerr.get_feature_names()
+    
+    return feature_names, X.toarray()
+
+# vectorizer to dataframe
+def get_dataframe(X, feature_names):
+    df = pd.DataFrame(data = X, columns = feature_names)
+    return df
+
+# odd word functions
+
+def get_numeric_words(feature_names):
+    stops = set()
+    nums = [str(num) for num in range(10)]
+    
+    for word in feature_names:
+        for char in word:
+            if char in nums:
+                stops.add(word)
+                break
+                
+    return list(stops)
+
+def get_non_alpha_start(feature_names):
+    non_alpha = []
+    alpha = list(string.ascii_lowercase)
+    for name in feature_names:
+        if name[0] not in alpha:
+            non_alpha.append(name)
+            
+    return non_alpha
+
+def get_underscores(feature_names):
+    unders = []
+    for name in feature_names:
+        if '_' in name:
+            unders.append(name)
+            
+    return unders
+
+def get_multis(feature_names):
+    multi = []
+    for name in feature_names:
+        if len(name) == 2:
+            if name[0] == name[1]:
+                multi.append(name)
+        else:
+            search = re.search(r"([A-Za-z])\1\1", name)
+            if search != None:
+                multi.append(name)
+                
+    return multi
+
+# wrap for odd words
+def get_special_stops(feature_names):
+    numeric = get_numeric_words(feature_names)
+    non_alpha = get_non_alpha_start(feature_names)
+    unders = get_underscores(feature_names)
+    multi_lett = get_multis(feature_names)
+    
+    return [numeric, non_alpha, unders, multi_lett]
+
+
+# PIPELINE FOR Counts:
+
+def load_samps(num_samples=250000):
+    chyrons = pd.read_csv('chyron_all.csv') # read all data
+    chyrons.drop(472372, inplace=True) # drop single NaN value
+    chy_samp = get_random_sample(chyrons, num_samples) # random sample for working locally 
+    
+    return chy_samp
+    
+def clean_counts(chy_samp):
+    count_features, chyron_counts = get_countvec(chy_samp['text'], stop_words='english') # get primary feature names
+    stops = get_special_stops(count_features) # get additional stop words
+    stop_words_plus = get_stop_words(stops) # add to nltk stop words
+    count_features, chyron_counts = get_countvec(chy_samp['text'], stop_words=stop_words_plus) # get new vector matrix
+
+    return count_features, chyron_counts
